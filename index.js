cbd1a94e69ce5f9586c81962923afc15ff93374f
ohidulalam <mdnoyonctg15@gmail.com>
2022-03-14 21:40:19 +0600

    others post setup

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const ObjectId = require("mongodb").ObjectId;
-const { ObjectID } = require("bson");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.5qzra.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-console.log(uri);
+const uri =
+  "mongodb+srv://islami-bank-ltd:OHpWryxttf7kMr2N@cluster0.5qzra.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 async function server() {
   try {
     await client.connect();
     const database = client.db("ibbl");
-    const personalDetailsCollection = database.collection("personalDetails");
-    const businessDetailsCollection = database.collection("businessDetails");
+    const PersonaInfoCollection = database.collection("PersonaInfo");
+    const businessInfoCollection = database.collection("businessInfo");
     const loanApplicationDetailsCollection = database.collection(
       "loanApplicationDetails"
     );
 
-    app.post("/projectBuild", async (req, res) => {
-      const project = req.body;
-      const result = await projectsCollection.insertOne(project);
+    app.post("/personalDetails", async (req, res) => {
+      const personalDetails = req.body;
+      const result = await PersonaInfoCollection.insertOne(personalDetails);
+      res.json(result);
+    });
+    app.post("/businessDetails", async (req, res) => {
+      const businessDetails = req.body;
+      const result = await businessInfoCollection.insertOne(businessDetails);
+      res.json(result);
+    });
+    app.post("/loanApplicationDetails", async (req, res) => {
+      const loanApplicationDetails = req.body;
+      const result = await loanApplicationDetailsCollection.insertOne(
+        loanApplicationDetails
+      );
       res.json(result);
     });
   } finally {
